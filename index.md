2d3d463297a9c9c3d64ff51ec27359d2791bc1ed
Kalen Jordan <kalen@magemail.co>
2016-01-14 20:11:38 -0800

    Markdown the home page content

---
+++ index.md
@@ -0,0 +1,9 @@
+**goodlinks.io**
+
+Hey there,
+
+What's crackin'? Hey so I wanted to let you know about this new
+project that I'm starting to work on.
+
+Thanks,
+<br/>Kalen
---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once('vendor/michelf/php-markdown/Michelf/MarkdownExtra.inc.php');
+use \Michelf\MarkdownExtra;
+
+?>
+
+<html>
+    <head>
+        <style type="text/css">
+            body {
+                padding: 1em;
+                font-family: courier;
+                font-size: 1.3em;
+                background-color: #F5F5F5;
+            }
+
+            .content {
+                background-color: white;
+                padding: 1em;
+                max-width: 26em;
+                margin-left: auto;
+                margin-right: auto;
+            }
+
+            .content p {
+                line-height: 1.4em;
+                margin: 0;
+                padding: 0;
+                margin-bottom: 0.75em;
+            }
+
+            .content p:nth-last-child(1) {
+                margin-bottom: 0;
+            }
+        </style>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+<body>
+    <div class="content">
+        <?php
+        echo MarkdownExtra::defaultTransform(file_get_contents('index.md'));
+        ?>
+    </div>
+</body>
+</html>
+
