8d2a48ec454b26962e1ab2e1fe08f11b3a2e1f14
Kalen Jordan <kalen@magemail.co>
2016-01-22 08:41:27 -0800

    Add WIP favicon (sorry Antonela, don't kill me!)

--- index.php
+++ index.php
@@ -66,6 +66,7 @@ use \Michelf\MarkdownExtra;
             }
         </style>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="/image/goodlinks-favicon.png">
         <script>
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
