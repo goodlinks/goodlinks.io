8d59d8d2499918124ccd511be809d3eb195d30df
Kalen Jordan <kalen@magemail.co>
2016-01-29 00:11:06 -0800

    Move the Oauth class into buzzstream-feed lib, clean up logic a bit

--- buzzstream-feed.php
+++ buzzstream-feed.php
@@ -14,30 +14,8 @@ $Loader->putenv();
 Api::setConsumerKey(getenv('BUZZSTREAM_CONSUMER_KEY'));
 Api::setConsumerSecret(getenv('BUZZSTREAM_CONSUMER_SECRET'));
 
-// This is going to be moved into a class file and eventually
-// replaced with a nice clean (league?) php package
-require_once('Oauth.php');
+$history = History::getList();
+print_r($history);
+foreach ($history as $historyItem) {
 
-$consumer_key = Api::getConsumerKey();
-$consumer_secret = Api::getConsumerSecret();
-$base_url = "https://api.buzzstream.com";
-
-#Create a consumer for the buzzstream api
-$consumer = new OAuthConsumer($consumer_key, $consumer_secret);
-
-#From the consumer create and sign a request object for 2leg authentication
-$request = OAuthRequest::from_consumer_and_token($consumer, NULL, "GET", $base_url . "/v1/history");
-$sig_method = new OAuthSignatureMethod_HMAC_SHA1();
-$request->sign_request($sig_method, $consumer, NULL);
-
-#Perform request
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, $request->to_url());
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_HTTPHEADER, array($request->to_header()));
-$response = curl_exec($curl);
-curl_close($curl);
-
-#Print response
-$json_response = json_decode($response, true);
-print_r($json_response['list']);
+}
