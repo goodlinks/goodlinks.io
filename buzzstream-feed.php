d7a5fcab79905c0fe337b3d8c8ceb833f27f2322
Kalen Jordan <kalen@magemail.co>
2016-02-01 03:52:09 -0800

    Add history item summary and a url key parameter for auth

--- buzzstream-feed.php
+++ buzzstream-feed.php
@@ -15,15 +15,24 @@ $Loader->putenv();
 Api::setConsumerKey(getenv('BUZZSTREAM_CONSUMER_KEY'));
 Api::setConsumerSecret(getenv('BUZZSTREAM_CONSUMER_SECRET'));
 
+$key = isset($_GET['key']) ? $_GET['key'] : null;
+if (!$key || $key != getenv('WEB_KEY')) {
+    die("Access denied");
+}
+
 $offset = isset($_GET['offset']) ? $_GET['offset'] : null;
-$size = isset($_GET['size']) ? $_GET['size'] : null;
+$size = isset($_GET['size']) ? $_GET['size'] : 50;
 
 $history = History::getList($offset, $size);
+
+echo "<title>BuzzStream Feed</title>";
 
 foreach ($history as $historyItem) {
     /** @var $historyItem HistoryItem */
     $date = $historyItem->getDate();
     $websiteUrls = $historyItem->getWebsiteNamesCsv();
     $project = $historyItem->getProjectName();
-    echo "<br>$project - $date: $websiteUrls";
+    $summary = $historyItem->getSummary();
+    
+    echo "<br>$project ($date) $websiteUrls - $summary";
 }
